620f3fad44e4a2e116376dd5e65b615ed2e403b4
Eric Wellnitz <me@ericwellnitz.com>
2018-07-18 12:30:46 -0400

    wrapped httpclient in a using to make sure it's disposed

--- DotNetODataClient/DotNetODataClient/Program.cs
+++ DotNetODataClient/DotNetODataClient/Program.cs
@@ -16,24 +16,25 @@ namespace DotNetODataClient
             string userName = "user@university-a.campusnexus.cloud";
             string password = "password";
 
-            var httpClient = new HttpClient();
-
-            //set authentication header
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
-                Convert.ToBase64String(Encoding.ASCII.GetBytes($"{userName}:{password}")));
-
-            //invoke HTTP Get
-            var response = httpClient.GetAsync($"{rootUri}ds/odata/Courses").Result;
-            response.EnsureSuccessStatusCode();
-
-            //deserialize response into a JObject
-            var result = JsonConvert.DeserializeObject<JObject>(response.Content.ReadAsStringAsync().Result);
-
-            //write values to console
-            foreach (var child in result["value"])
+            using (var httpClient = new HttpClient())
             {
-                Console.WriteLine(child["Code"]);
-                Console.WriteLine(child["Name"]);
+                //set authentication header
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                    Convert.ToBase64String(Encoding.ASCII.GetBytes($"{userName}:{password}")));
+
+                //invoke HTTP Get
+                var response = httpClient.GetAsync($"{rootUri}ds/odata/Courses").Result;
+                response.EnsureSuccessStatusCode();
+
+                //deserialize response into a JObject
+                var result = JsonConvert.DeserializeObject<JObject>(response.Content.ReadAsStringAsync().Result);
+
+                //write values to console
+                foreach (var child in result["value"])
+                {
+                    Console.WriteLine(child["Code"]);
+                    Console.WriteLine(child["Name"]);
+                }
             }
 
             Console.ReadLine();
